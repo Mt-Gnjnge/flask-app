72f7b8974f53701af6890f7a53d3d97c41706756
Mt-Gnjnge <yamken624@gmail.com>
2025-06-08 22:46:47 +0900

    SQLiteに保存できるようにした

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-from flask import Flask,render_template, request
+from flask import Flask, request, redirect, render_template
+import sqlite3
 
 app=Flask(__name__)
 tasks=[]
 
+def get_db_connection():
+    conn=sqlite3.connect('database.db')
+    conn.row_factory=sqlite3.Row # 結果を辞書っぽく扱える
+    return conn
+
 @app.route("/", methods=["GET", "POST"])
-def home():
+def index():
+    conn=get_db_connection()
     # name = None
     if request.method == "POST":
         task = request.form["task"]
-        tasks.append(task)
+        conn.execute("INSERT INTO tasks (content) VALUES (?)", (task,))
+        conn.commit()
+        return redirect("/")
+    
+        # tasks.append(task)
         # name = request.form["username"]
+    tasks = conn.execute("SELECT * FROM tasks").fetchall()
+    conn.close()
     return render_template("index.html", tasks=tasks)
     
-# if __name__=="__main__":
-#     app.run(debug=True)
+if __name__=="__main__":
+    app.run(debug=True)
